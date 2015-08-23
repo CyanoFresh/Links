0a60e5b33d626f076e77c58913d8eb945ef886fe
Alex Solomaha <cyanofresh@gmail.com>
2015-08-23 16:54:27 +0300

    Fixes and additions

---
+++ views/site/api.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'API';
+?>
+
+<section class="section--center mdl-grid">
+    <div class="card-wide mdl-card mdl-shadow--2dp">
+        <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">API</h2>
+        </div>
+        <div class="mdl-card__supporting-text">
+            <p>To be done....</p>
+        </div>
+    </div>
+</section>
